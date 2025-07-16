f1f494773516aaea20e6113dac7f9ac83053498f
Rakshit rajput <Rakshitrajput212@gmail.com>
2025-07-16 11:04:46 +0530

    feat:initial setup -added user model -added reveiw model

---
+++ backend/models/user.js
@@ -0,0 +1,27 @@
+const mongoos = require("mongoose");
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 50,
+  },
+  emailId: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+const User = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
